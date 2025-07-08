24a780c975f4375ce497bcffc2e1fa2e8a185e13
Santoshkumar-S <Santoshkumar-S@users.noreply.github.com>
2025-07-09 00:40:31 +0530

    added complete frontend layout (HTML, CSS, JS)

--- frontend/script.js
+++ frontend/script.js
@@ -1,1 +1,12 @@
+function findDay() {
+  const input = document.getElementById("dateInput").value;
+  const result = document.getElementById("result");
 
+  if (!input) {
+    result.textContent = "Please select a date.";
+    return;
+  }
+0
+  const day = new Date(input).toLocaleDateString("en-US", { weekday: 'long' });
+  result.textContent = `That day is a ${day}`;
+}
